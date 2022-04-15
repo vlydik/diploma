dfcc04b06683dc3d0acffc490f03167565e25971
vldktsn <vlad.K2001@gmail.com>
2022-04-15 17:07:34 +0300

    Fix bugs

--- src/components/UI/Main/Main.js
+++ src/components/UI/Main/Main.js
@@ -57,18 +57,18 @@ const Main = () => {
             <a>Reliable speed. 24/7 Support. All the time.</a>
         </section>
         <div className={classes.product__cards}>
-            <div class={classes.card}>
+            <div className={classes.card}>
                 <h1>Standard</h1>
-                <p class={classes.price}>30zl/month</p>
+                <p className={classes.price}>30zl/month</p>
                 <b>100 Mb/s</b>
                 <p>Services included:</p>
                 <b>Internet Analysis</b>
                 <br/><br/><br/>
                 <p><button>Join now</button></p>
             </div>
-            <div class={classes.card}>
+            <div className={classes.card}>
                 <h1>Pro</h1>
-                <p class={classes.price}>60zl/month</p>
+                <p className={classes.price}>60zl/month</p>
                 <b>200 Mb/s</b>
                 <p>Services included:</p>
                 <b>Internet Analysis</b>
@@ -78,9 +78,9 @@ const Main = () => {
                 <b>Parental Control</b>
                 <p><button>Join now</button></p>
             </div>
-            <div class={classes.card}>
+            <div className={classes.card}>
                 <h1>Enterprise</h1>
-                <p class={classes.price}>from 100zl/month</p>
+                <p className={classes.price}>from 100zl/month</p>
                 <b>500 Mb/s</b>
                 <p>Services included:</p>
                 <b>Expand internet capacity</b><br/>
