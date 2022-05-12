95fbbec3bd9d416bb930f2d105053d889d5e59cd
vldktsn <vlad.K2001@gmail.com>
2022-05-12 21:32:45 +0300

    I have fixed something but I don't know what I fixed

---
+++ src/components/UI/HomePageUI/Plans/PackageList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import classes from "./PackageList.module.css";
+
+const PackageList = (props) => {
+    if(props.items.length === 0){
+        return <h2>Found no packages. Seems something is wrong.</h2>
+    }
+
+    return(
+        <ul className={classes.planList}>
+            {props.items.map((plan)=>{
+                <Plan
+                    key={plan.id}
+                    nameOfPackage={plan.nameOfPackage}
+                    price={plan.price}
+                    speed={plan.speed}
+                />
+            })}
+        </ul>
+    )
+}
